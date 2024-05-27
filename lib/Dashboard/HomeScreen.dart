c01abd4d7bcf173cdad889b71e9155b1fdf73b35
Ahmedsab9912 <ahmedsab9912@gmail.com>
2024-05-27 18:07:01 +0500

    Resturant API DONE

--- lib/Dashboard/HomeScreen.dart
+++ lib/Dashboard/HomeScreen.dart
@@ -1,14 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import '../BookingScreens/Bookings.dart';
 import '../Models/AllRestaurantsAPI.dart';
 import 'package:http/http.dart' as http;
-import '../NotificationScreen/NotificationPage.dart';
-import '../PartyScreens/partydemoscreen.dart';
-import '../Profile.dart';
-import '../SavedScreen/savedscreen.dart';
-
 
 class ToggleIcon extends StatefulWidget {
   const ToggleIcon({super.key});
@@ -30,15 +24,15 @@ class _ToggleIconState extends State<ToggleIcon> {
       },
       child: _isFilled
           ? SvgPicture.asset(
-        'assets/svgIcons/filledHeart.svg', // Path to the filled SVG icon
-        width: 24,
-        height: 24,
-      )
+              'assets/svgIcons/filledHeart.svg', // Path to the filled SVG icon
+              width: 24,
+              height: 24,
+            )
           : SvgPicture.asset(
-        'assets/svgIcons/emptyHeart.svg', // Path to the unfilled SVG icon
-        width: 24,
-        height: 24,
-      ),
+              'assets/svgIcons/emptyHeart.svg', // Path to the unfilled SVG icon
+              width: 24,
+              height: 24,
+            ),
     );
   }
 }
@@ -55,7 +49,6 @@ class _HomescreenState extends State<Homescreen> {
   List<Data> searchlistModel = [];
   TextEditingController search_Controller = TextEditingController();
 
-
   @override
   void initState() {
     super.initState();
@@ -63,7 +56,8 @@ class _HomescreenState extends State<Homescreen> {
   }
 
   Future<void> fetchRestaurants() async {
-    var response = await http.get(Uri.parse('http://192.168.43.54:8000/restaurants'));
+    var response =
+        await http.get(Uri.parse('http://192.168.43.54:8000/restaurants'));
 
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
@@ -83,12 +77,10 @@ class _HomescreenState extends State<Homescreen> {
     setState(() {
       searchlistModel = restaurantListModel
           .where((restaurant) =>
-          restaurant.name!.toLowerCase().contains(searchKey.toLowerCase()))
+              restaurant.name!.toLowerCase().contains(searchKey.toLowerCase()))
           .toList();
     });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -220,85 +212,88 @@ class _HomescreenState extends State<Homescreen> {
               child: restaurantListModel.isEmpty
                   ? Center(child: CircularProgressIndicator())
                   : ListView.builder(
-                itemCount: searchlistModel.length,
-                itemBuilder: (context, index) {
-                  var restaurant = searchlistModel[index];
-                  return Padding(
-                    padding: EdgeInsets.only(right: 10, left: 10, top: 10),
-                    child: Card(
-                      elevation: 3,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Image from the internet
-                            Center(
-                              child: Image.network(
-                                'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D',
-                                height: 220,
-                                width: 350,
-                                fit: BoxFit.cover,
+                      itemCount: searchlistModel.length,
+                      itemBuilder: (context, index) {
+                        var restaurant = searchlistModel[index];
+                        return Padding(
+                          padding:
+                              EdgeInsets.only(right: 10, left: 10, top: 10),
+                          child: Card(
+                            elevation: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  // Image from the internet
+                                  Center(
+                                    child: Image.network(
+                                      'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHxlbnwwfHwwfHx8MA%3D%3D',
+                                      height: 220,
+                                      width: 350,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      // Restaurant name
+                                      Text(
+                                        restaurant.name ?? "No Name",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 25,
+                                            color: Colors.green),
+                                      ),
+                                      // Like icon
+                                      ToggleIcon(),
+                                    ],
+                                  ),
+                                  SizedBox(height: 5),
+                                  // Restaurant city
+                                  Text(
+                                    restaurant.city ?? "No City",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  SizedBox(height: 5),
+                                  // Restaurant state
+                                  Text(
+                                    restaurant.address ?? "No State",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      // Rating text
+                                      Text(
+                                        "Rating: 4",
+                                        style: TextStyle(
+                                            fontSize: 14, color: Colors.black),
+                                      ),
+                                      // Star rating
+                                      Row(
+                                        children: List.generate(5, (starIndex) {
+                                          return Icon(
+                                            Icons.star,
+                                            color: starIndex < 4
+                                                ? Colors.yellow
+                                                : Colors.grey,
+                                          );
+                                        }),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
-                            SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                // Restaurant name
-                                Text(
-                                  restaurant.name ?? "No Name",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 25,color: Colors.green),
-                                ),
-                                // Like icon
-                                ToggleIcon(),
-                              ],
-                            ),
-                            SizedBox(height: 5),
-                            // Restaurant city
-                            Text(
-                              restaurant.city ?? "No City",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            SizedBox(height: 5),
-                            // Restaurant state
-                            Text(
-                              restaurant.address ?? "No State",
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                            SizedBox(height: 10),
-                            Row(
-                              mainAxisAlignment:
-                              MainAxisAlignment.spaceBetween,
-                              children: [
-                                // Rating text
-                                Text(
-                                  "Rating: 4",
-                                  style: TextStyle(
-                                      fontSize: 14, color: Colors.black),
-                                ),
-                                // Star rating
-                                Row(
-                                  children: List.generate(5, (starIndex) {
-                                    return Icon(
-                                      Icons.star,
-                                      color: starIndex < 4
-                                          ? Colors.yellow
-                                          : Colors.grey,
-                                    );
-                                  }),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             ),
           ],
         ),
